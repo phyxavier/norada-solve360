949c37e5e9397e6eded28f11b20ee84a94740d88
Bernard Otu <bernard.otu@galagoco.com>
2015-03-04 23:19:16 -0500

    changed folders

--- lib/solve360.rb
+++ lib/solve360.rb
@@ -3,9 +3,3 @@ require 'rubygems'
 require 'json'
 
 Dir["#{File.dirname(__FILE__)}/norada/solve360/*.rb"].each { |source_file| require source_file }
-
-module Norada
-  module Solve360
-    # Your code goes here...
-  end
-end
