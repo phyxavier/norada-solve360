5e9ef1150bac7251ffc73f3a8dde5f41e21b451c
Bernard Otu <bernard.otu@galagoco.com>
2015-03-04 23:56:24 -0500

    update

--- lib/solve360.rb
+++ lib/solve360.rb
@@ -2,4 +2,4 @@ require "norada/solve360/version"
 require 'rubygems'
 require 'json'
 
-Dir["#{File.dirname(__FILE__)}/norada/solve360/*.rb"].each { |source_file| require source_file }
+Dir["#{File.dirname(__FILE__)}/solve360/*.rb"].each { |source_file| require source_file }
